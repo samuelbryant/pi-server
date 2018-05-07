c1b26811b67e1bfd2885bb02f9ea600a3e67eab9
sambryant <samuel.bryant@yale.edu>
2018-05-07 13:42:53 -0400

    Have semi-working script. About to work on adding a config file

--- local_scripts/sns_backup.py
+++ local_scripts/sns_backup.py
@@ -1,21 +1,33 @@
 # In progress. Eventually this will be part of a cron job.
 import syslog
 import atexit
+import os
 from subprocess import call
 
-# TODO: Move this all into a configuration file.
 DRYRUN = True
+
+# TODO:
+# These are constants that are specific to each backup set. 
+# Eventually these should be dynamically read from a file so this script can
+# be reused for many files.
 JOBNAME='blade-pi-SNS-backup'
-PI_HOST = 'sjb-pi-ext'
 SOURCE_DIR = '/home/sambryant/'
-DEST_DIR = 'sambryant@sjb-pi-ext:/drives/data/blade/home_sambryant'
+DEST_DIR = '/drives/data/blade/home_sambryant'
+IGNORE_FILES = ['/.cache', '/.config/chromium']
+
+# TODO:
+# These are global constants that are spcific to the entire pi-server project.
+# These should be read from a config file.
+PI_HOST = 'sjb-pi-ext'
+PI_USER = 'sambryant'
+TARGET_DIR = '%s@%s:%s' % (PI_USER, PI_HOST, DEST_DIR)
+#'sambryant@sjb-pi-ext:/drives/data/blade/home_sambryant'
 SERVER_LOGGER = 'python3 /drives/data/pi-server/server_scripts/log_server.py'
 OPTIONS = ['-a', '-v', '--delete']
-IGNORE_FILES = ['/.cache', '/.config/chromium']
 EXPIRE_TIME = '2000'
 
 # Build actual rsync command
-rsync_command = ['sudo', 'rsync', SOURCE_DIR, DEST_DIR]
+rsync_command = ['rsync', SOURCE_DIR, TARGET_DIR]
 for option in OPTIONS:
   rsync_command.append(option)
 for ignore in IGNORE_FILES:
@@ -58,7 +70,7 @@ def main():
     nonlocal completed_flag
     if completed_flag != 1:
       shortmsg = 'Unknown failure during SNS backup'
-      longmsg = 'Script terminated prematurely (caught by atexit function'
+      longmsg = 'Script terminated prematurely (caught by atexit function.)'
       notify_desktop(shortmsg, longmsg, persist=True, severe=True)
       notify_local('\n'.join([shortmsg, longmsg]), iserror=True)
       notify_server('\n'.join([shortmsg, longmsg]), iserror=True)
@@ -69,23 +81,25 @@ def main():
   # else.
 
   # Start backup
-  shortmsg = 'SNS backup started'
-  longmsg = 'Copying data from %s to %s' % (SOURCE_DIR, DEST_DIR)
+  shortmsg = 'SNS backup started (dry run = %s)' % str(DRYRUN)
+  longmsg = 'Copying data from %s to %s' % (SOURCE_DIR, TARGET_DIR)
   notify_all(shortmsg, longmsg)
 
   print(' '.join(rsync_command))
   code = call(rsync_command)
 
   if code == 0:
-    shortmsg = 'SNS backup finished successfully'
-    longmsg = 'Successfully copied data from %s to %s' % (SOURCE_DIR, DEST_DIR)
+    shortmsg = 'SNS backup finished successfully (dry run = %s)' % str(DRYRUN)
+    longmsg = 'Successfully copied data from %s to %s' % (SOURCE_DIR, TARGET_DIR)
     notify_all(shortmsg, longmsg)
   else:
-    shortmsg = 'SNS backup failed with code %d' % code
-    longmsg = '\nFailed to backup data from %s to %s\nFailed command: %s' % (
-      SOURCE_DIR, DEST_DIR, ' '.join(rsync_command))
+    shortmsg = 'SNS backup failed with code %d (dry run = %s)' % (code, str(DRYRUN))
+    longmsg = 'Failed to backup data from %s to %s\nFailed command: %s' % (
+      SOURCE_DIR, TARGET_DIR, ' '.join(rsync_command))
     notify_all(shortmsg, longmsg, iserror=True)
   completed_flag = 1
 
 if __name__ == '__main__':
+  user = os.environ['USER']
+  syslog.syslog('SJB TEST: sjb-pi: user: '+str(user))
   main()
