04cbd2a1ddbc03959e49f37c49e98ce5b86f9f6a
sambryant <samuel.bryant@yale.edu>
2018-05-07 13:10:10 -0400

    Script updates

--- local_scripts/sns_backup.py
+++ local_scripts/sns_backup.py
@@ -1,47 +1,91 @@
 # In progress. Eventually this will be part of a cron job.
 import syslog
+import atexit
 from subprocess import call
 
-JOBNAME='pi-SNS-backup'
-
+# TODO: Move this all into a configuration file.
+DRYRUN = True
+JOBNAME='blade-pi-SNS-backup'
+PI_HOST = 'sjb-pi-ext'
 SOURCE_DIR = '/home/sambryant/'
 DEST_DIR = 'sambryant@sjb-pi-ext:/drives/data/blade/home_sambryant'
+SERVER_LOGGER = 'python3 /drives/data/pi-server/server_scripts/log_server.py'
 OPTIONS = ['-a', '-v', '--delete']
 IGNORE_FILES = ['/.cache', '/.config/chromium']
+EXPIRE_TIME = '2000'
 
-def open_log():
-  syslog.openlog(JOBNAME)
-
-def write_to_log(msg):
-  syslog.syslog(msg)
-
-def notify_desktop(msg):
-  call(['notify-send', 'SNS Backup: ' + msg])
-
-def print_cmd_stack(cmds):
-  print(' '.join(cmds))
-
-cmd_stack = ['sudo', 'rsync', SOURCE_DIR, DEST_DIR]
-
+# Build actual rsync command
+rsync_command = ['sudo', 'rsync', SOURCE_DIR, DEST_DIR]
 for option in OPTIONS:
-  cmd_stack.append(option)
+  rsync_command.append(option)
 for ignore in IGNORE_FILES:
-  cmd_stack.append('--exclude='+ignore)
+  rsync_command.append('--exclude='+ignore)
+if DRYRUN:
+  rsync_command.append('-n')
 
-# First we check that everything exists.
+def notify_desktop(summary, body, persist=False, severe=False):
+  # TODO: Find out what happens on call failure and if we can recover from it.
+  stack = ['notify-send', summary, body]
+  if not persist:
+    stack.append('-t')
+    stack.append(EXPIRE_TIME)
+  if severe:
+    stack.append('-u critical')
+  #print('notify command: '+' '.join(stack))
+  call(stack)
 
+def notify_server(msg, iserror=False):
+  stack = ['ssh', PI_HOST, SERVER_LOGGER, '"'+JOBNAME+'"', '"'+msg+'"']
+  if iserror:
+    stack.append('-s 2')
+  code=call(stack)
+  print('server logging code: '+str(code))
 
-# Next we check lock and assert lock (TODO: Semaphores, posix inter-process)
+def notify_local(msg, iserror=False):
+  syslog.openlog(JOBNAME)
+  syslog.syslog(syslog.LOG_ERR if iserror else syslog.LOG_INFO, msg)
 
+def notify_all(shortmsg, longmsg, iserror=False):
+  notify_desktop(shortmsg, longmsg, persist=iserror, severe=iserror)
+  notify_local('%s: %s' % (shortmsg, longmsg), iserror=iserror)
+  notify_server('%s: %s' % (shortmsg, longmsg), iserror=iserror)
 
-# Notify desktop
-notify_desktop('Starting backup job')
-write_to_log('starting SNS backup job')
-print_cmd_stack(cmd_stack)
-return_code = call(cmd_stack)
-write_to_log('finished SNS backup job with code '+str(return_code))
-notify_desktop('Finished backup job with code '+str(return_code))
+def main():
 
-# print('Finished with code: '+str(return_code))
+  # Add cleanup code in case the script terminates prematurely:
+  completed_flag = 0
+  def on_exit_cleanup():
+    nonlocal completed_flag
+    if completed_flag != 1:
+      shortmsg = 'Unknown failure during SNS backup'
+      longmsg = 'Script terminated prematurely (caught by atexit function'
+      notify_desktop(shortmsg, longmsg, persist=True, severe=True)
+      notify_local('\n'.join([shortmsg, longmsg]), iserror=True)
+      notify_server('\n'.join([shortmsg, longmsg]), iserror=True)
+  atexit.register(on_exit_cleanup)
 
-# Finally we release lock
+  # TODO: add multi-process semaphore locking. To ensure that two backup jobs 
+  # dont conflict, we should try to obtain a lock here before doing anything 
+  # else.
+
+  # Start backup
+  shortmsg = 'SNS backup started'
+  longmsg = 'Copying data from %s to %s' % (SOURCE_DIR, DEST_DIR)
+  notify_all(shortmsg, longmsg)
+
+  print(' '.join(rsync_command))
+  code = call(rsync_command)
+
+  if code == 0:
+    shortmsg = 'SNS backup finished successfully'
+    longmsg = 'Successfully copied data from %s to %s' % (SOURCE_DIR, DEST_DIR)
+    notify_all(shortmsg, longmsg)
+  else:
+    shortmsg = 'SNS backup failed with code %d' % code
+    longmsg = '\nFailed to backup data from %s to %s\nFailed command: %s' % (
+      SOURCE_DIR, DEST_DIR, ' '.join(rsync_command))
+    notify_all(shortmsg, longmsg, iserror=True)
+  completed_flag = 1
+
+if __name__ == '__main__':
+  main()
--- server_scripts/log_server.py
+++ server_scripts/log_server.py
@@ -1,26 +1,53 @@
 import syslog
 import sys
 import argparse
+import time
+import datetime
 # Script used to make log entries on the server
 # This should be called using SSH from the client. e.g.:
 #  ssh sjb-pi-ext .../log_server.py <job name> <msg>
 
-
 USAGE = '(ssh sjb-pi-ext python3) log_server.py <jobname> <message>'
 
+# rasp pi doesn't have a syslog by default so we will use our own log.
+LOG_FILE = '/drives/data/logs/backup_info.log'
+ERR_FILE = '/drives/data/logs/backup_fail.log'
+
+SEVERITY_LABELS = {
+  0: 'MSG',
+  1: 'WRN',
+  2: 'ERR'
+}
 
 def main():
   parser = argparse.ArgumentParser(
     description='Log to server\'s syslog from client', usage=USAGE)
   parser.add_argument(
-      'jobname', type=str, help='The jobname to identify entries in syslog')
+      'jobname', type=str, help='The jobname to identify entries the log')
   parser.add_argument(
-      'message', type=str, help='The message to log in server\'s syslog')
+      'message', type=str, help='The message to log in server side log')
+  parser.add_argument(
+      '-s', dest='severity', type=int, choices=[0,1,2],
+      default=0,
+      help='Severity level of log entry (0 = message, 1 = warning, 2 = error)')
   args = parser.parse_args(sys.argv[1:])
 
-  syslog.openlog(args.jobname)
-  syslog.syslog(args.message)
+
+  timestamp = datetime.datetime.strftime(datetime.datetime.now(), '%y-%m-%d %H:%M:%S')
+
+  msg = '%s [%s] [%s]: %s\n' % (
+    timestamp, SEVERITY_LABELS[args.severity], args.jobname, args.message)
+
+  logf = open(LOG_FILE, 'a')
+  logf.write(msg)
+  logf.close()
+
+  if args.severity != 0:
+    errf = open(ERR_FILE, 'a')
+    errf.write(msg)
+    errf.close()
+
   exit(0)
 
-
-
+if __name__ == '__main__':
+  main()
