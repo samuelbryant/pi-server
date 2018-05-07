ffc8168c5ce79be773c3a5b8f5d9f68ee25ab4d9
sambryant <samuel.bryant@yale.edu>
2018-05-07 16:49:30 -0400

    Added shitty temporary install script just so I dont accidentally mess something up

---
+++ scripts/temp_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Moves scripts to places in the system so that they are "installed" in a way 
+# that keeps track of all changes so they can be reverted later once a proper
+# install method is devised
+
+
+src="$1"
+dst="$2"
+
+if [[ "$#" -ne "2" ]]; then
+  echo "Usage: $0 <src script> <dst>"
+  exit 1
+fi
+
+if [[ ! -e "$src" ]]; then
+  echo "Error: source doesnt exist"
+  exit 1
+fi
+
+if [[ -e "$dst" ]]; then
+  echo "Error: dest already exists"
+  exit 1
+fi
+
+sudo cp "$src" "$dst"
+parent=$(dirname "$0")
+host=$(hostname)
+echo "[$host]: cp '$src' '$dst'" >> "$parent/install_list.txt"
