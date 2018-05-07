504eaa67c313f2a4feb94b613ec1126514c09c1d
sambryant <samuel.bryant@yale.edu>
2018-05-07 11:39:02 -0400

    Wrote actual server side log script

--- server_scripts/log_server.py
+++ server_scripts/log_server.py
@@ -1,14 +1,26 @@
 import syslog
 import sys
+import argparse
 # Script used to make log entries on the server
 # This should be called using SSH from the client. e.g.:
-#  ssh sjb-pi-ext:/.../log_server.py <job name> <msg>
+#  ssh sjb-pi-ext .../log_server.py <job name> <msg>
 
 
-USAGE = 'ssh sjb-pi-ext python3 log_server.py <job name> <msg>'
+USAGE = '(ssh sjb-pi-ext python3) log_server.py <jobname> <message>'
 
 
+def main():
+  parser = argparse.ArgumentParser(
+    description='Log to server\'s syslog from client', usage=USAGE)
+  parser.add_argument(
+      'jobname', type=str, help='The jobname to identify entries in syslog')
+  parser.add_argument(
+      'message', type=str, help='The message to log in server\'s syslog')
+  args = parser.parse_args(sys.argv[1:])
 
-for arg in sys.argv:
-  print('Arg: '+str(arg))
+  syslog.openlog(args.jobname)
+  syslog.syslog(args.message)
+  exit(0)
+
+
 
