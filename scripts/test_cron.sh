0c5bb33d598ffd6af8b7e2637cd795bd4a9be9d8
sambryant <samuel.bryant@yale.edu>
2018-05-09 11:51:38 -0400

    Still testing cron/systemd on server

--- scripts/test_cron.sh
+++ scripts/test_cron.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "test test text test" >> /home/sambryant/test_cron_out.txt
+ts=$(date)
+echo "This is a test script from /etc/cron.hourly/test_cron.sh." >> /home/sambryant/cron_test_README.txt
+echo "This script was executed at $ts" >> /home/sambryant/cron_test_README.txt
