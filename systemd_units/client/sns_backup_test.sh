8a4116db7872f352073aa2a97e12708ce60e2f7b
sambryant <samuel.bryant@yale.edu>
2018-05-11 12:48:54 -0400

    Removed outdated systemd stuff

--- systemd_units/client/sns_backup_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ts=$(date)
-echo "this systemd unit was set up at 7:00pm may 8" > "/home/sambryant/systemd_README.txt"
-echo "This systemd unit was excuted at $ts" >> "/home/sambryant/systemd_README.txt"
--- systemd_units/test.sh
+++
@@ -1,2 +0,0 @@
-echo "hello test systemd timer test yes yes yes" >> /home/sambryant/systemd_timer_test.txt
-echo "hello test systemd timer test yes yes yes" >> /tmp/systemd_timer_test.txt
